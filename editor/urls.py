7f229ad321bb8dafe6a739061c0c5af62da7ebc8
Kensuke Kousaka <k@k3n.link>
2017-10-28 21:28:00 +0900

    Add sample html -> jquery ajax post -> django views -> jquery ajax callback

--- editor/urls.py
+++ editor/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from editor import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^compile/', views.compile_content, name='compile_content')
 ]
--- editor/views.py
+++ editor/views.py
@@ -7,3 +7,17 @@ def index(request):
     return render_to_response(template_name='index.jinja', context={
         'user': request.user
     })
+
+
+@login_required
+def compile_content(request):
+    import json
+    from django.http import HttpResponse,Http404
+
+    if request.method == 'POST':
+        content = request.POST['content']
+        print('content: '+content)
+        response = json.dumps({'result': 'OK'})
+        return HttpResponse(response, content_type='text/javascript')
+    else:
+        raise Http404
